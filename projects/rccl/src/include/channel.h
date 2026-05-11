6e2b183112dcab88839c618cf8e8b46567e80e32
isaki001 <ioannissakiotis@gmail.com>
2026-05-11 18:52:36 +0000

    gate batchSize increase by enablement of pxn

--- projects/rccl/src/include/channel.h
+++ projects/rccl/src/include/channel.h
@@ -24,7 +24,7 @@ inline uint8_t ncclP2pChannelBaseForRound(struct ncclComm* comm, int p2pRound, i
   if (comm->nNodes > 1) {
     int nodeDelta = p2pRound/comm->maxLocalRanks;
     int localDelta = p2pRound%comm->maxLocalRanks;
-    int fallbackBatch = (rcclParamPxnOptQpUsage() && rcclUseAinic()) ? 8 : 1;
+    int fallbackBatch = (!ncclPxnDisable(comm) && rcclParamPxnOptQpUsage() && rcclUseAinic()) ? comm->maxLocalRanks : 1;
     int batchSize = (comm->nNodes > 2 && p2pBatchEnable)
         ? NCCL_MAX_DEV_WORK_P2P_PER_BATCH
         : fallbackBatch;
