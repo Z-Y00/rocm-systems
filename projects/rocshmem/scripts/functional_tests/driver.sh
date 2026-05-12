1b3b2fa13ad31b90fea2f55353d0e785649d592f
Edgar Gabriel <Edgar.Gabriel@amd.com>
2026-05-12 12:09:52 -0500

    make functional tests pass again

--- projects/rocshmem/scripts/functional_tests/driver.sh
+++ projects/rocshmem/scripts/functional_tests/driver.sh
@@ -637,6 +637,23 @@ TestOther() {
   ExecTest  "shmemptr"         2       16           128       8
 }
 
+TestTiles() {
+  ##############################################################################
+  #       | Name                      | Ranks | Workgroups | Threads | Max Message Size #
+  ##############################################################################
+  ExecTest  "tile_put_contiguous"       2       1            1
+  ExecTest  "tile_put_rowmajor"         2       1            1
+  ExecTest  "tile_put_colmajor"         2       1            1
+  ExecTest  "tile_put_arbitrary"        2       1            1
+  ExecTest  "tile_put_wave_contiguous"  2       1            64
+  ExecTest  "tile_put_wg_contiguous"    2       1            1024
+  ExecTest  "tile_get_contiguous"       2       1            1
+  ExecTest  "tile_get_wg_contiguous"    2       1            1024
+  ExecTest  "tile_put_1d"               2       1            1
+  ExecTest  "tile_get_1d"               2       1            1
+  ExecTest  "tile_get_wave_contiguous"  2       1            64
+}
+
 TestHeatMapRMA() {
   NOTIMEOUT=1
   NOVERIF=1
@@ -803,6 +820,10 @@ case $TEST in
     TestColl
     TestOther
     TestOnStream
+    # Tile tests are only supported on IPC backend
+    if [[ ! "$TEST" =~ ^(gda|ro) ]]; then
+      TestTiles
+    fi
     ;;
   *"rma")
     TestRMA
@@ -827,6 +848,9 @@ case $TEST in
     ;;
   *"other")
     TestOther
+    ;;
+  *"tiles")
+    TestTiles
     ;;
   *)
     #######################################################################################
