bf3a60f80f398388b7a8e8ec9a6ff20b4af3b9c5
Mikhail <lloginov@amd.com>
2026-05-15 09:21:12 -0400

    rank-based log filtering

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library.cpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library.cpp
@@ -548,6 +548,18 @@ rocprofsys_init_library_hidden()
     // configure the settings
     configure_settings();
 
+    // Disable Timemory console output for specified ranks
+    if(!config::output_filtering::is_log_output_enabled_for_current_mpi_rank())
+    {
+        auto* _settings = tim::settings::instance();
+        if(_settings)
+        {
+            _settings->cout_output() = false;
+            _settings->verbose()     = -1;
+            _settings->banner()      = false;
+        }
+    }
+
     auto _debug_value = get_debug();
     if(_debug_init) config::set_setting_value("ROCPROFSYS_DEBUG", true);
     scope::destructor _debug_dtor{ [_debug_value, _debug_init]() {
@@ -594,17 +606,17 @@ rocprofsys_init_tooling_hidden(void)
 
     if(_debug_init)
     {
-        LOG_DEBUG("Printing banner...");
-    }
-
-    print_banner();
-
-    if(_debug_init)
-    {
         LOG_DEBUG("Calling rocprofsys_init_library()...");
     }
 
     rocprofsys_init_library_hidden();
+
+    if(_debug_init)
+    {
+        LOG_DEBUG("Printing banner...");
+    }
+
+    print_banner();
 
     auto _dtor = scope::destructor{ []() {
         // if set to finalized, don't continue
@@ -1212,7 +1224,7 @@ rocprofsys_finalize_hidden(void)
             settings::default_process_suffix() = fmt::format("%pid%-{}", session_id++);
 
         // Disable Timemory file output for disabled ranks
-        if(!config::output_filtering::is_output_enabled_for_current_mpi_rank())
+        if(!config::output_filtering::is_file_output_enabled_for_current_mpi_rank())
         {
             auto* _settings = tim::settings::instance();
             if(_settings)
@@ -1252,7 +1264,10 @@ rocprofsys_finalize_hidden(void)
         }
     }
 
-    _output_registry.print_summary();
+    if(config::output_filtering::is_log_output_enabled_for_current_mpi_rank())
+    {
+        _output_registry.print_summary();
+    }
 
     categories::shutdown();
 
